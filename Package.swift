576c3187c4465fc9f536b9510fbc0dad9b3e9188
night_vision <ilya.a.ashirov@gmail.com>
2026-08-11 23:51:09 -0400

    Misc

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -34,5 +34,6 @@ let package = Package(
             name: "Profiler",
             dependencies: ["DiskVisualizerLib"]
         )
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
